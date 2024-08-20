1f6f77fc612dfcf07724e795584b6baceebca5d3
Ahmad Jaenal Aripin <ahmadjaenal629@gmail.com>
2024-08-20 11:20:14 +0700

    [^] Slicing UI gallery

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,62 +1,66 @@
 <footer id="footer" class="footer dark-background">
 
     <div class="container">
-      <div class="row gy-3">
-        <div class="col-lg-3 col-md-6 d-flex">
-          <i class="bi bi-geo-alt icon"></i>
-          <div class="address">
-            <h4>Address</h4>
-            <p>A108 Adam Street</p>
-            <p>New York, NY 535022</p>
-            <p></p>
-          </div>
+        <div class="row gy-3">
+            <div class="col-lg-4 col-md-6 d-flex">
+                <div class="address">
+                    <h3 style="font-weight: bold;">TASTY FOOD</h3>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aperiam non natus illo perspiciatis
+                        amet nihil esse enim recusandae. Cum in ducimus quae consectetur repellat placeat ea iste
+                        laborum eveniet commodi.</p>
+                    <br>
+                    <div class="social-links d-flex">
+                        <a href="#" class="twitter"><img src="../img/icons/icon_twitter_big.png" alt=""
+                                width="32px"></a>
+                        <a href="#" class="facebook"><img src="../img/icons/icon_fb_big.png" alt=""
+                                width="32px"></a>
+                    </div>
+                </div>
 
+            </div>
+
+            <div class="col-lg-2 col-md-6 d-flex">
+                <div>
+                    <h3>Useful links</h3>
+                    <p>Blog</p>
+                    <p>Hewan</p>
+                    <p>Galeri</p>
+                    <p>Testimonial</p>
+                </div>
+            </div>
+
+            <div class="col-lg-2 col-md-6 d-flex">
+                <div>
+                    <h3>Privacy</h3>
+                    <p>Karir</p>
+                    <p>Tentang Kami</p>
+                    <p>Kontak Kami</p>
+                    <p>Servis</p>
+                </div>
+            </div>
+
+            <div class="col-lg-3 col-md-6 d-flex">
+                <div>
+                    <h3>Contact Info</h3>
+                    <p>
+                        <img src="../img/icons/icon_message_big.png" alt="" width="14px;">
+                        <span>tastyfood@gmail.com</span><br>
+                    </p>
+                    <p>
+                        <img src="../img/icons/icon_call_big.png" alt="" width="14px;">
+                        <span>+6836743434</span><br>
+                    </p>
+                    <p>
+                        <img src="../img/icons/icon_place_big.png" alt="" width="14px;">
+                        <span>Kota Bandung, Jawa Barat</span><br>
+                    </p>
+                </div>
+            </div>
         </div>
-
-        <div class="col-lg-3 col-md-6 d-flex">
-          <i class="bi bi-telephone icon"></i>
-          <div>
-            <h4>Contact</h4>
-            <p>
-              <strong>Phone:</strong> <span>+1 5589 55488 55</span><br>
-              <strong>Email:</strong> <span>info@example.com</span><br>
-            </p>
-          </div>
-        </div>
-
-        <div class="col-lg-3 col-md-6 d-flex">
-          <i class="bi bi-clock icon"></i>
-          <div>
-            <h4>Opening Hours</h4>
-            <p>
-              <strong>Mon-Sat:</strong> <span>11AM - 23PM</span><br>
-              <strong>Sunday</strong>: <span>Closed</span>
-            </p>
-          </div>
-        </div>
-
-        <div class="col-lg-3 col-md-6">
-          <h4>Follow Us</h4>
-          <div class="social-links d-flex">
-            <a href="#" class="twitter"><i class="bi bi-twitter-x"></i></a>
-            <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
-            <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
-            <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></a>
-          </div>
-        </div>
-
-      </div>
     </div>
 
     <div class="container copyright text-center mt-4">
-      <p>© <span>Copyright</span> <strong class="px-1 sitename">Yummy</strong> <span>All Rights Reserved</span></p>
-      <div class="credits">
-        <!-- All the links in the footer should remain intact. -->
-        <!-- You can delete the links only if you've purchased the pro version. -->
-        <!-- Licensing information: https://bootstrapmade.com/license/ -->
-        <!-- Purchase the pro version with working PHP/AJAX contact form: [buy-url] -->
-        Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
-      </div>
+        <p>© <span>Copyright</span> <strong class="px-1 sitename"></strong> <span>All Rights Reserved</span></p>
     </div>
 
-  </footer>
+</footer>
--- resources/views/partials/navbar-transparent.blade.php
+++ resources/views/partials/navbar-transparent.blade.php
@@ -4,7 +4,7 @@
       <a href="index.html" class="logo d-flex align-items-center me-auto me-xl-0">
         <!-- Uncomment the line below if you also wish to use an image logo -->
         <!-- <img src="img/logo.png" alt=""> -->
-        <h1 class="sitename">Yummy</h1>
+        <h1 class="sitename">TASTY FOOD</h1>
         <span>.</span>
       </a>
 
