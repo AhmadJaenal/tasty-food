a2b81294fd448c018598444084578370ba623e8f
Ahmad Jaenal Aripin <ahmadjaenal629@gmail.com>
2024-08-20 10:52:42 +0700

    [+] Add new routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ContactController;
+use App\Http\Controllers\GalleryController;
+use App\Http\Controllers\LandingPageController;
+use App\Http\Controllers\NewsController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('index');
-});
+Route::get('/', [LandingPageController::class, 'landingPage'])->name('landingPage');
+Route::get('/about', [AboutController::class, 'about'])->name('about');
+Route::get('/news', [NewsController::class, 'news'])->name('news');
+Route::get('/gallery', [GalleryController::class, 'gallery'])->name('gallery');
+Route::get('/contact', [ContactController::class, 'contact'])->name('contact');
