782c8a449a5ca8857e5a69e2d00d54086f7e8eb5
Ahmad Jaenal Aripin <ahmadjaenal629@gmail.com>
2024-08-22 09:45:06 +0700

    [+] Add fetch news data from news table; Add feat delete news;

--- app/Http/Controllers/NewsDashboardController.php
+++ app/Http/Controllers/NewsDashboardController.php
@@ -2,12 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\News;
 use Illuminate\Http\Request;
 
 class NewsDashboardController extends Controller
 {
     public function newsDashboard()
     {
-        return view('dashboard.pages.news.news');
+        $news = News::all();
+        return view('dashboard.pages.news.news', compact('news'));
+    }
+
+    public function deleteNews($id_news)
+    {
+        try {
+            News::find($id_news)->delete();
+            return redirect()->route('newsDashboard')->with('successDelete', 'Data Berhasil Dihapus');
+        } catch (\Throwable $th) {
+            return redirect()->route('newsDashboard')->with('failedDelete', 'Data Gagal Dihapus');
+        }
     }
 }
