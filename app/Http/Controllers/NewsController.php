1df3c575aba2c0348379506aa65b56c9e507116c
Ahmad Jaenal Aripin <ahmadjaenal629@gmail.com>
2024-08-22 09:44:21 +0700

    [+] Add fetch news data from news table

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\News;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class NewsController extends Controller
 {
     public function news()
     {
-        return view("landingPage.pages.news");
+        $news = News::all();
+        return view("landingPage.pages.news", compact('news'));
     }
 }
