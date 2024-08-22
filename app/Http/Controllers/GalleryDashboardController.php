3993b8b329ae90f4df4680aff618e1e51c977397
Ahmad Jaenal Aripin <ahmadjaenal629@gmail.com>
2024-08-22 09:43:08 +0700

    [+] Add fetch gallery data; Add feat delete image;

--- app/Http/Controllers/GalleryDashboardController.php
+++ app/Http/Controllers/GalleryDashboardController.php
@@ -2,12 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Gallery;
 use Illuminate\Http\Request;
 
 class GalleryDashboardController extends Controller
 {
     public function galleryDashboard()
     {
-        return view('dashboard.pages.gallery.gallery');
+        try {
+            $images = Gallery::all();
+            return view('dashboard.pages.gallery.gallery', compact('images'));
+        } catch (\Throwable $th) {
+            return view('dashboard.pages.gallery.gallery');
+        }
+    }
+
+    public function deleteImage($id_image)
+    {
+        try {
+            Gallery::find($id_image)->delete();
+            return redirect()->route('galleryDashboard')->with('successDelete', 'Successfully deleted the image');
+        } catch (\Throwable $th) {
+            return redirect()->route('galleryDashboard')->with('failedDelete', 'Failed to delete image');
+        }
+    }
+
+    public function addImage() 
+    {
+        return view('dashboard.pages.gallery.add_image');
     }
 }
