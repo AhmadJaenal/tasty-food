c3881795796eb8c9b10a3583b471f09ca3573407
Ahmad Jaenal Aripin <ahmadjaenal629@gmail.com>
2024-08-22 09:42:32 +0700

    [+] Add function fetch gallery data

--- app/Http/Controllers/GalleryController.php
+++ app/Http/Controllers/GalleryController.php
@@ -2,12 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Gallery;
 use Illuminate\Http\Request;
 
 class GalleryController extends Controller
 {
     public function gallery()
     {
-        return view("landingPage.pages.gallery");
+        try {
+            $gallery = Gallery::all();
+            return view("landingPage.pages.gallery", compact('gallery'));
+        } catch (\Throwable $th) {
+            return view("landingPage.pages.gallery");
+        }
     }
 }
