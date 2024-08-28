5d45682e1afec67c42f5bac7ed865873c661be0d
Ahmad Jaenal Aripin <ahmadjaenal629@gmail.com>
2024-08-28 12:54:23 +0700

    Feat [+] Add send email

---
+++ app/Mail/sendEmail.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class sendEmail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $dataEmail;
+
+    /**
+     * Create a new message instance.
+     */
+    public function __construct($dataEmail)
+    {
+        $this->dataEmail = $dataEmail;
+    }
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: $this->subject,
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function build()
+    {
+        return $this->from($this->dataEmail['email'], $this->dataEmail['name'])
+            ->subject($this->dataEmail['subject'])
+            ->view('landingPage.pages.mail.sendMail')
+            ->with([
+                'messageContent' => $this->dataEmail['message']
+            ]);
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array
+    {
+        return [];
+    }
+}
