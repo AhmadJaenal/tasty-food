1931aaea0c890bd3bffd603dde1306d94684eb71
Ahmad Jaenal Aripin <ahmadjaenal629@gmail.com>
2024-08-28 12:48:38 +0700

    [+] Add fetch image slider

--- app/Http/Controllers/GalleryController.php
+++ app/Http/Controllers/GalleryController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Gallery;
+use App\Models\Slider;
 use Illuminate\Http\Request;
 
 class GalleryController extends Controller
@@ -10,8 +11,9 @@ class GalleryController extends Controller
     public function gallery()
     {
         try {
+            $imageSlider = Slider::all();
             $gallery = Gallery::all();
-            return view("landingPage.pages.gallery", compact('gallery'));
+            return view("landingPage.pages.gallery", compact('gallery', 'imageSlider'));
         } catch (\Throwable $th) {
             return view("landingPage.pages.gallery");
         }
